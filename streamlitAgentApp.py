62aabb09afe67b048ac9421f8d3e7c2a5067de6e
ayush1111 <lokareaayush@gmail.com>
2024-04-23 22:43:20 +0530

    UI changes

--- streamlitAgentApp.py
+++ streamlitAgentApp.py
@@ -28,15 +28,18 @@ def input_fields():
         provider_options = ['anyscale', 'together-ai']
         
         
-        llm1_selected_option = st.selectbox("Select LLM 1", llm_options)
-        provider1_selected_option = st.selectbox("Select Provider 1", provider_options)
-        llm2_selected_option = st.selectbox("Select LLM 2", llm_options)
-        provider2_selected_option = st.selectbox("Select Provider 2", provider_options)
+        llm_selected_option = st.selectbox("Select LLM ", llm_options)
+        provider_selected_option = st.selectbox("Select Provider ", provider_options)
         
-        st.session_state.llm1 = llm1_selected_option
-        st.session_state.llm2 = llm2_selected_option
-        st.session_state.provider1 = provider1_selected_option
-        st.session_state.provider2 = provider2_selected_option
+        # llm1_selected_option = st.selectbox("Select LLM 1", llm_options)
+        # provider1_selected_option = st.selectbox("Select Provider 1", provider_options)
+        # llm2_selected_option = st.selectbox("Select LLM 2", llm_options)
+        # provider2_selected_option = st.selectbox("Select Provider 2", provider_options)
+        
+        st.session_state.llm = llm_selected_option
+        # st.session_state.llm2 = llm2_selected_option
+        st.session_state.provider = provider_selected_option
+        # st.session_state.provider2 = provider2_selected_option
         # if "pinecone_api_key" in st.secrets:
         #     st.session_state.pinecone_api_key = st.secrets.pinecone_api_key
         # else: 
@@ -63,10 +66,10 @@ async def a_query_llm(llm, provider,query):
     return responses
 
 
-async def fetch_responses(query):
-    response1 = a_query_llm(st.session_state.llm1, st.session_state.provider1, query)
-    response2 = a_query_llm(st.session_state.llm2, st.session_state.provider2, query)
-    return await asyncio.gather(response1, response2)
+# async def fetch_responses(query):
+#     response1 = a_query_llm(st.session_state.llm1, st.session_state.provider1, query)
+#     response2 = a_query_llm(st.session_state.llm2, st.session_state.provider2, query)
+#     return await asyncio.gather(response1, response2)
 
 def query_llm(llm, provider, query):
     endpoint = f"{llm}@{provider}"  
@@ -84,54 +87,83 @@ def boot():
     
     # st.button("Submit Documents", on_click=process_documents)
     
-    #store messages in a list
-    if "messages1" not in st.session_state:
-        st.session_state.messages1 = []
+    # store messages in a list
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+        
+    # if "messages1" not in st.session_state:
+    #     st.session_state.messages1 = []
     
-    if "messages2" not in st.session_state:
-        st.session_state.messages2 = []
+    # if "messages2" not in st.session_state:
+    #     st.session_state.messages2 = []
     
     
     # if query := st.chat_input():
 
-    #     st.session_state.messages1.append(("You", query))
+    #     st.session_state.messages.append(("You", query))
+        # st.session_state.messages1.append(("You", query))
     #     st.session_state.messages2.append(("You", query))
     
+    st.subheader(f"{st.session_state.llm}@{st.session_state.provider}")
+    for message in st.session_state.messages:
+        st.chat_message('You').write(message[0])
+        st.chat_message('LLM').write(message[1])       
+    # col1, col2 = st.columns(2)
+    # st.subheader(f"{st.session_state.llm1}@{st.session_state.provider1}")
+    # for message in st.session_state.messages1:
+    #     st.chat_message('You').write(message[0])
+    #     st.chat_message('1').write(message[1])      
+    # with col1:
+    #     st.subheader(f"{st.session_state.llm1}@{st.session_state.provider1}")
+    #     for message in st.session_state.messages1:
+    #         st.chat_message('You').write(message[0])
+    #         st.chat_message('1').write(message[1])   
     
-    col1, col2 = st.columns(2)
-    
-    with col1:
-        st.subheader(f"{st.session_state.llm1}@{st.session_state.provider1}")
-        for message in st.session_state.messages1:
-            st.chat_message('You').write(message[0])
-            st.chat_message('1').write(message[1])   
-    
-    with col2:  
-        st.subheader(f"{st.session_state.llm2}@{st.session_state.provider2}")
-        for message in st.session_state.messages2:
-            st.chat_message('You').write(message[0])
-            st.chat_message('2').write(message[1])    
+    # with col2:  
+    #     st.subheader(f"{st.session_state.llm2}@{st.session_state.provider2}")
+    #     for message in st.session_state.messages2:
+    #         st.chat_message('You').write(message[0])
+    #         st.chat_message('2').write(message[1])    
 
     if query := st.chat_input(): 
-        responses = asyncio.run(fetch_responses(query))
+        # responses = asyncio.run(fetch_responses(query))
+        # responses = asyncio.run(fetch_responses(query))
+        responses = query_llm(st.session_state.llm, st.session_state.provider, query)
          # Update the message histories
-        st.session_state.messages1.append((query, responses[0]))
-        st.session_state.messages2.append((query, responses[1]))
- 
-        with col1:
-            st.chat_message("You").write(query)
+        st.session_state.messages.append((query, responses))
+        # st.session_state.messages1.append((query, responses[0]))
+        # st.session_state.messages2.append((query, responses[1]))
+
+        st.chat_message("You").write(query)
+        st.chat_message("LLM").write(responses)
+        # with col1:
+        #     st.chat_message("You").write(query)
             
-            # response1 = query_llm(st.session_state.llm1,st.session_state.provider1, query)
-            st.chat_message("1").write(responses[0])
+        #     # response1 = query_llm(st.session_state.llm1,st.session_state.provider1, query)
+        #     st.chat_message("1").write(responses[0])
 
             
-        with col2:
-            st.chat_message("You").write(query)
-            # response2 = query_llm(st.session_state.llm2,st.session_state.provider2, query)
+        # with col2:
+        #     st.chat_message("You").write(query)
+        #     # response2 = query_llm(st.session_state.llm2,st.session_state.provider2, query)
             
-            st.chat_message("2").write(responses[1])
+        #     st.chat_message("2").write(responses[1])
 
-
+    # input_fields()
+    # #
+    # st.button("Submit Documents", on_click=process_documents)
+    # #
+    # if "messages" not in st.session_state:
+    #     st.session_state.messages = []
+    # #
+    # for message in st.session_state.messages:
+    #     st.chat_message('human').write(message[0])
+    #     st.chat_message('ai').write(message[1])    
+    # #
+    # if query := st.chat_input():
+    #     st.chat_message("human").write(query)
+    #     response = query_llm(st.session_state.retriever, query)
+    #     st.chat_message("ai").write(response)
 
 
 
